aef444e67dfd3919a8f80409b022f19c2ebb2db7
Tonyeseh <esehtony123@gmail.com>
2024-01-09 11:13:51 +0100

    task 0 - 5 0x01 promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,5 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()]).then((res) => console.log(`${res[0].body} ${res[1].firstName} ${res[1].firstName}`));
+}
