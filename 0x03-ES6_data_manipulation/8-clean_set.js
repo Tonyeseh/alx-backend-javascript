df8bd2c6015a46f7655b005b2ff30ab329c7e8f1
Tonyeseh <esehtony123@gmail.com>
2024-01-12 11:48:23 +0100

    correction task 8 0x03

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
-  if (startString !== '') {
+  if (typeof startString === 'string' && startString !== '') {
     const array = Array.from(set);
-    const array1 = array.filter((elem) => elem.startsWith(startString));
+    const array1 = array.filter((elem) => typeof elem === 'string' && elem.startsWith(startString));
     const array2 = array1.map((elem) => elem.slice(startString.length));
     return array2.join('-');
   }
