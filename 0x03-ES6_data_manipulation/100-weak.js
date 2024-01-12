eec2596f391a06f0a013ec221a776a7b663d654a
Tonyeseh <esehtony123@gmail.com>
2024-01-12 11:51:19 +0100

    correction to task adv 11 0x03

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,6 +4,6 @@ export function queryAPI(endPoint) {
   const count = weakMap.get(endPoint);
   weakMap.set(endPoint, count ? count + 1 : 1);
   if (count && count >= 5) {
-    throw new Error('Endpoing load is high');
+    throw new Error('Endpoint load is high');
   }
 }
