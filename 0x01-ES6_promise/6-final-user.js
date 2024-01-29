3aae2082f2b00e3613c6fe3ede7e109df1d26632
Tonyeseh <esehtony123@gmail.com>
2024-01-29 23:34:39 +0100

    task 6 0x01 correction

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,16 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((data) => {
+      const results = [];
+
+      for (const d of data) {
+        results.push({
+          status: d.status,
+          value: d.status === 'fulfilled' ? d.value : String(d.reason),
+        });
+      }
+      return results;
+    });
 }
