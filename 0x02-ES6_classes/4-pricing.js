6884afc7115af36782d4a5c6643eed4c3eef80ab
Tonyeseh <esehtony123@gmail.com>
2024-01-30 01:01:11 +0100

    task 4 0x02 complete

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,31 +1,34 @@
-import Currency from "./3-currency";
-
+import Currency from './3-currency';
 
 export default class Pricing {
-    constructor (amount, currency) {
-        this.amount = amount;
-        this.currency = currency;
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() { return this._amount; }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new Error('amount must a number');
     }
+    this._amount = value;
+  }
 
-    get amount() { return this._amount; }
-    set amount(value) { 
-        if (typeof value !== "number") {
-            throw new Error("amount must a number");
-        }
-        this._amount = value; }
+  get currency() { return this._currency; }
 
-    get currency() { return this._currency;}
-    set currency(value) { 
-        if (value.constructor.name !== "Currency") {
-            throw new Error("currency must a Currency Object")
-        }
-        this._currency = value; }
-
-    displayFullPrice () {
-        return `${this._amount} ${this._currency.displayFullCurrency}`
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new Error('currency must a Currency Object');
     }
+    this._currency = value;
+  }
 
-    static convertPrice(amount, conversionRate) {
-        return amount * conversionRate;
-    }
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
