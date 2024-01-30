03350516da5706578f029ea2b4815d42b62bb68c
Tonyeseh <esehtony123@gmail.com>
2024-01-30 01:11:57 +0100

    task 5 0x02 complete

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,15 +1,16 @@
 export default class Building {
-    constructor (sqft) {
-        if (typeof sqft !== 'number') {
-            throw new TypeError('sqft must be a number')
-        }
-        this._sqft = sqft;
-
-        if (this.constructor !== Building) {
-            if (typeof this.evacuationWarningMessage !== 'function') {
-                throw new Error('Class extending Building must override evacuationWarningMessage')
-            }
-        }
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
     }
-    get sqft () { return this._sqft; }
+    this._sqft = sqft;
+
+    if (this.constructor.name !== 'Building') {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+  }
+
+  get sqft() { return this._sqft; }
 }
