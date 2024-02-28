9f4854ed6c4d23e3669e7642c3c62811b1b5fcf5
Tonyeseh <esehtony123@gmail.com>
2024-02-28 05:34:12 +0100

    task 5 0x05

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,25 @@
+const app = require('http');
+const argv = process.argv;
+const countStudents = require('./3-read_file_async');
+
+app.createServer(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.write('Hello Holberton School!');
+    res.end();
+  }
+  if (req.url === '/students') {
+    res.statusCode = 200;
+    res.write('This is the list of our students\n');
+
+    countStudents(argv[2]).then((data) => {
+      res.write(data.join('\n'));
+      res.end();
+    }).catch((error) => {
+      res.write(error.message);
+      res.end();
+    });
+  }
+}).listen(1245);
