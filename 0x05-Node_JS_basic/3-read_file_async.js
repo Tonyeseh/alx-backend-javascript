f272886590d473dadb1273da29ada6d77a6fe6d3
Tonyeseh <esehtony123@gmail.com>
2024-02-28 05:50:13 +0100

    updated task 3 0x05

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,6 +1,7 @@
 const fsPromise = require('fs').promises;
 
 async function countStudents (fileName) {
+  let result = []
   try {
     const data = await fsPromise.readFile(fileName, 'utf8');
     const dataArray = data.split('\n').slice(1, -1).filter((row) => row !== '');
@@ -13,6 +14,8 @@ async function countStudents (fileName) {
       return row[row.length - 1];
     }));
 
+    result.push(`Number of students: ${dataArray.length}`)
+
     console.log(`Number of students: ${dataArray.length}`);
 
     for (const field of uniqueFields) {
@@ -20,11 +23,14 @@ async function countStudents (fileName) {
 
       const firstNames = anotherArray.map((row) => row[0]);
 
+      result.push(`Number of students in ${field}: ${anotherArray.length}. List: ${firstNames.join(', ')}`)
+
       console.log(`Number of students in ${field}: ${anotherArray.length}. List: ${firstNames.join(', ')}`);
     }
   } catch (error) {
     throw new Error('Cannot load the database');
   }
+  return result
 }
 
 module.exports = countStudents;
